95a0445b55d01a007864aee2f9392c1d8f765af1
Gunnar Bastkowski <gunnar@bastkowski.name>
2018-08-09 18:52:02 +0200

    Move towards functional code with return values

--- src/main/scala/gbastkowski/kata/eventsourcing/Invoice.scala
+++ src/main/scala/gbastkowski/kata/eventsourcing/Invoice.scala
@@ -3,7 +3,8 @@ package gbastkowski.kata.eventsourcing
 import java.time.LocalDateTime
 import scala.collection.mutable
 
-case class Invoice(id: Option[Int] = None) {
+case class Invoice(
+  id: Option[Int] = None) {
   val items: mutable.ListBuffer[InvoiceItem] = mutable.ListBuffer()
   private[this] var totalAmount: Int = 0
   var recipient: Option[String] = None
@@ -11,33 +12,39 @@ case class Invoice(id: Option[Int] = None) {
   var reminded: Option[LocalDateTime] = None
   var paymentReceived: Option[LocalDateTime] = None
 
-  def changeRecipient(recipient: Option[String]): Unit = {
+  def changeRecipient(recipient: Option[String]): Invoice = {
     this.recipient = recipient
+    this
   }
 
-  def addItem(description: String, amount: Int): Unit = {
+  def addItem(description: String, amount: Int): Invoice = {
     require(sent.isEmpty)
 
     items += new InvoiceItem(description, amount)
     totalAmount += amount
+    this
   }
 
-  def removeItem(item: InvoiceItem): Unit = {
+  def removeItem(item: InvoiceItem): Invoice = {
     require(sent.isEmpty)
 
     items -= item
     totalAmount -= item.amount
+    this
   }
 
-  def send(): Unit = {
+  def send(): Invoice = {
     sent = Some(LocalDateTime.now())
+    this
   }
 
-  def remind(): Unit = {
+  def remind(): Invoice = {
     reminded = Some(LocalDateTime.now())
+    this
   }
 
-  def paymentReceived(when: LocalDateTime): Unit = {
+  def paymentReceived(when: LocalDateTime): Invoice = {
     paymentReceived = Some(when)
+    this
   }
 }
--- src/test/scala/gbastkowski/kata/eventsourcing/InvoiceSpec.scala
+++ src/test/scala/gbastkowski/kata/eventsourcing/InvoiceSpec.scala
@@ -8,34 +8,30 @@ class InvoiceSpec extends FreeSpec with Matchers {
       val invoice = new Invoice()
 
       "can add an item" in {
-        invoice.addItem("Coke", 1)
-        invoice.items should have size 1
+        invoice.addItem("Coke", 1).items should have size 1
       }
 
       "can remove an item" in {
-        invoice.removeItem(invoice.items(0))
-        invoice.items shouldBe empty
+        invoice.removeItem(invoice.items(0)).items shouldBe empty
       }
 
       "can change it's recipient" in {
-        invoice.changeRecipient(Some("Recipient"))
+        invoice.changeRecipient(Some("Recipient")).recipient shouldBe Some("Recipient")
       }
 
       "can be sent" in {
-        invoice.send()
-        invoice.sent shouldBe defined
+        invoice.send().sent shouldBe defined
       }
 
       "can be reminded" in {
-        invoice.remind()
-        invoice.reminded shouldBe defined
+        invoice.remind().reminded shouldBe defined
       }
     }
 
     "which has been sent" - {
-      val invoice = new Invoice()
-      invoice.addItem("coke", 2)
-      invoice.send()
+      val invoice = new Invoice().
+        addItem("coke", 2).
+        send()
 
       "cannot add items" in {
         an[IllegalArgumentException] should be thrownBy invoice.addItem("a", 1)
