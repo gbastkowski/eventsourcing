f138510a50be45a279c488ea01da5e0faf06201d
Gunnar Bastkowski <gunnar@bastkowski.name>
2018-08-09 18:17:56 +0200

    Add Invoice.removeItem

--- src/main/scala/gbastkowski/kata/eventsourcing/Invoice.scala
+++ src/main/scala/gbastkowski/kata/eventsourcing/Invoice.scala
@@ -7,9 +7,15 @@ class Invoice {
   val items: mutable.ListBuffer[InvoiceItem] = mutable.ListBuffer()
   var recipient: Option[String] = None
 
-  def changeRecipient(recipient: Option[String]): Unit = this.recipient = recipient
+  def changeRecipient(recipient: Option[String]): Unit = {
+    this.recipient = recipient
+  }
 
   def addItem(description: String, amount: Int): Unit = {
     items += new InvoiceItem(description, amount)
+  }
+
+  def removeItem(item: InvoiceItem): Unit = {
+    items -= item
   }
 }
--- src/test/scala/gbastkowski/kata/eventsourcing/InvoiceSpec.scala
+++ src/test/scala/gbastkowski/kata/eventsourcing/InvoiceSpec.scala
@@ -7,13 +7,23 @@ class InvoiceSpec extends FreeSpec with Matchers {
     "can be created" in {
       new Invoice()
     }
+
     "can add an item" in {
       val invoice = new Invoice()
       invoice.addItem("Coke", 1)
       invoice.items should have size 1
     }
+
+    "can remove an item" in {
+      val invoice = new Invoice()
+      invoice.addItem("Coke", 1)
+      invoice.removeItem(invoice.items(0))
+      invoice.items shouldBe empty
+    }
+
     "can change it's recipient" in {
       val invoice = new Invoice()
-      invoice.changeRecipient("Recipient")
+      invoice.changeRecipient(Some("Recipient"))
+    }
   }
 }
